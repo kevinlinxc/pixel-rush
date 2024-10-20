74246de14f96dc2644a584bd174b15105df84f41
Kevin Lin <kevinlinxc@gmail.com>
2024-10-19 23:31:41 -0700

    Attempt Github Pages publish

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: "/pixel-brush",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+  };
+  
+  module.exports = nextConfig;
+  
